599aeb7e07d95eae1a1bd5b461dcc68f1b2d84f1
helloiamtok <helloiamtok@gmail.com>
2021-06-04 16:50:28 +0700

    Add Project Excise4
    Add Project Excise4

---
+++ Exercise4_Patinya_S.py
@@ -0,0 +1,9 @@
+FoundationEnglish = 84.7
+GeneralBusiness = 95.2
+ComputerSystem = 98.7
+ComputerPrograming = 93.5
+print ("---Your Score---")
+print ("Foundation English : " + str (FoundationEnglish))
+print ("General Business   : " + str (GeneralBusiness))
+print ("Introduction to Computer System : " + str (ComputerSystem))
+print ("Computer Programing : " + str (ComputerPrograming))
