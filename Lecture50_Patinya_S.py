d64cd0167e5fbd2ceee8263709b78f415f873182
helloiamtok <helloiamtok@gmail.com>
2021-06-11 02:15:08 +0700

    Create Lecture50_Patinya_S.py

---
+++ Lecture50_Patinya_S.py
@@ -0,0 +1,16 @@
+def add (number1,number2) :
+    print (number1+number2)
+
+def minus (number1,number2) :
+    print (number1-number2)
+
+def multiply (number1,number2) :
+    print (number1*number2)
+
+def devide (number1,number2) :
+    print (number1/number2)
+
+add(1,2)
+minus(1,2)
+multiply(1,2)
+devide(1,2)
